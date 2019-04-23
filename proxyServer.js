2f8f962ce0a84862c92231ac0f52f0ca31f28f2c
Jackson <jacksonrcohen@gmail.com>
2019-04-23 17:34:32 -0400

    Fix to support endpoints for new proxy service

--- proxyServer.js
+++ proxyServer.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
@@ -11,6 +12,12 @@ app.use(cors());
 
 app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.get('/items', (req, res) => {
+  axios.get('http://ec2-18-206-39-171.compute-1.amazonaws.com:3007/items')
+  .then(result => res.send(result.data))
+  .catch(err => console.log(err))
 })
 
 app.listen(port, () => {

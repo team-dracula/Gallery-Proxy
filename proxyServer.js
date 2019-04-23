20266b3267bdc489e7eee32d7fed23a5aed1e82e
Jackson <jacksonrcohen@gmail.com>
2019-04-23 17:11:13 -0400

    Fix for deployment

--- proxyServer.js
+++ proxyServer.js
@@ -1,24 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-
-app.use(cors({
-  'allowedHeaders': ['sessionId', 'Content-Type'],
-  'exposedHeaders': ['sessionId'],
-  'origin': '*',
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
-}));
 
 app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 app.listen(port, () => {
-  console.log(`server running on port: ${port}!`);
+  console.log(`Server running on port: ${port}!`);
 });

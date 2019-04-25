908c24014e38162eba369519550475e2bb271c91
Jackson <jacksonrcohen@gmail.com>
2019-04-25 15:17:10 -0400

    Prepared proxy for deployment

--- proxyServer.js
+++ proxyServer.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
@@ -12,13 +11,7 @@ app.use(cors());
 
 app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
-})
-
-app.get('/items', (req, res) => {
-  axios.get('http://ec2-18-206-39-171.compute-1.amazonaws.com:3007/items')
-  .then(result => res.send(result.data))
-  .catch(err => console.log(err))
-})
+});
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}!`);
